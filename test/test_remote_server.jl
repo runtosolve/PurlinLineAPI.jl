0764e41e55160980b9552558cef5efef12a8c4f8
Cris Moen <cris.moen@runtosolve.com>
2023-02-06 10:20:48 -0500

    get set up for Faizal

--- test/test_remote_server.jl
+++ test/test_remote_server.jl
@@ -15,11 +15,22 @@ support_locations = [0.0, 25.0*12]
 purlin_frame_connections = "bottom flange connection"
 bridging_locations =[ ]
 
+# using Pkg 
+# Pkg.add(url="https://github.com/runtosolve/AISIS100.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/LinesCurvesNodes.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/CUFSM.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/CrossSection.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/ThinWalledBeam.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/ThinWalledBeamColumn.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/InternalForces.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/ScrewConnections.jl.git")
+# Pkg.add(url="https://github.com/runtosolve/PurlinLine.jl.git")
+
 
 inputs = PurlinLine.Inputs(loading_direction, design_code, segments, spacing, roof_slope, cross_section_dimensions, material_properties, deck_details, deck_material_properties, frame_flange_width, support_locations, purlin_frame_connections, bridging_locations)
 inputs_json = JSON3.write(inputs)
 
 
-url = URI(scheme="http", host="157.245.87.161", port="8080",path = "/api/purlin_line")
+url = URI(scheme="http", host="157.245.87.161", port="8080", path="/api/purlin_line")
 
 resp = HTTP.post(url, [], inputs_json)
